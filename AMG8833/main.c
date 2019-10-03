31b4a307fc67b796cf3fe3996e55393b5a87e685
michelheil <49286214+michelheil@users.noreply.github.com>
2019-10-03 14:20:01 +0200

    Included ToDos and generalizes TWI_Read function

--- AMG8833/main.c
+++ AMG8833/main.c
@@ -9,6 +9,17 @@
  * Author : Michael
  */ 
 
+/*
+ToDo:
+- AMG8833 soldering
+- write general function to read out bytes with parameters: device/slave address, register address, expected bytes, buffer to store data
+- handle all Status
+- write individual function for sending start, stop signal through Control Register
+- Auslagerung in myTWI.h
+- define constants for 8x8 128 bytes gridRead
+- convert string of 64 uint16
+*/
+
 #define F_CPU                   16000000L
 
 #include <avr/io.h>
@@ -52,17 +63,29 @@
 #define AMG8833_SLA_W               ((AMG8833_SLAVE_ADDRESS << 1) | TW_WRITE) // shifted by one as the address is stored in the higher 7-bits
 #define AMG8833_SLA_R               ((AMG8833_SLAVE_ADDRESS << 1) | TW_READ)  // TW_WRITE (=0) and TW_READ (=1) are defined in util/twi.h
 
-#define AMG8833_THERMISTOR_BYTES    2 // Thermistor value consist of 2 bytes. bits T7..0 in lower byte and T10..8 in higher byte
-                                      // bit3 in higher byte carries sign (1=minus; 0=plus)
-#define AMG8833_TTHL                0x0E // Thermistor Temperature Register (lower level)
-#define AMG8833_TTHH                0x0F // Thermistor Temperature Register (higher level)
+// Thermistor Register
+#define AMG8833_THERMISTOR_BYTES    2       // Thermistor value consist of 2 bytes. bits T7..0 in lower byte and T10..8 in higher byte
+                                            // bit3 in higher byte carries sign (1=minus; 0=plus)
+#define AMG8833_TTHL                0x0E    // Thermistor Temperature Register (lower level)
+#define AMG8833_TTHH                0x0F    // Thermistor Temperature Register (higher level)
+
+// 8x8 Temperature Grid Register
+#define AMG8833_GRID_BYTES          128     // Thermistor value consist of 8x8 witch 2 bytes each. bits T7..0 in lower byte and T10..8 in higher byte
+                                            // bit3 in higher byte carries sign (1=minus; 0=plus)
+#define AMG8833_T01L                0x80    // Pixel 1 Temperature Register (lower level)
+#define AMG8833_T01H                0x81    // Pixel 1 Temperature Register (higher level)
+#define AMG8833_T64L                0xFE    // Pixel 64
+#define AMG8833_T64H                0xFF    // Pixel 64
+
+
 
 // setting TWAR is only required when the ATmega328p is in slave mode
 // TWAR = (AMG8833_SLAVE_ADDRESS << 1); // move one bit to left as bit0 of TWAR is used for General Call
-uint16_t temp = 0;
+float temp = 0;
 
-int16_t TWI_readThermistor();
-int TWI_readAMG8833ThermistorBytes(uint8_t * dest);
+int16_t TWI_readThermistor(void);
+float * TWI_readGrid(void);
+int     TWI_readAMG8833Bytes(uint8_t reg, uint8_t len, uint8_t * dest);
 
 
 int main(void)
@@ -72,13 +95,15 @@ int main(void)
 
         // start Cursor at Home
         LCD_setCursorHome();
-        LCD_sendDataString("Connecting...");
-        LCD_setCursorTo(0, 2);
+        LCD_sendDataString("Temperature:");
+        
     
     while (1) 
     {
-        temp = TWI_readThermistor();
+        LCD_setCursorTo(0, 2);
+        temp = 0.0265f * TWI_readThermistor();
         LCD_sendDataByte((uint8_t) temp);
+        LCD_sendDataString(" Celcius");
         
         _delay_ms(5000);
     }
@@ -87,32 +112,38 @@ int main(void)
 
 }
 
+
 // wrapper function for handing over buffer array to readAMG833ThermistorBytes
-int16_t TWI_readThermistor() {
+int16_t TWI_readThermistor(void) {
+    // initialize buffer for the two bytes of Thermistor
     uint8_t rawData[2] = {0, 0};
-    TWI_readAMG8833ThermistorBytes(&rawData[0]);
+
+    // read two bytes from Thermistor
+    TWI_readAMG8833Bytes(AMG8833_TTHL, AMG8833_THERMISTOR_BYTES, &rawData[0]);
+    
+    // return Thermistor bytes as one 16-bit variable
     return (int16_t) (((int16_t) rawData[1] << 8) | rawData[0]);
 }
 
 
 // github.com/jodalyst/AMG8833/blob/master/src/AMG8833.cpp
-void readGrid(float* tempValues) {
+// call method: float * tempGridValues = readGrid();
+float * TWI_readGrid(void) {
     
-    uint8_t rawGridData[128];
-    for (uint8_t i=0; i<128; i++){
-        rawGridData[i]=0;
-    }
+    static float tempValues[64];
+    uint8_t rawGridData[128] = {0};
 
-    TWI_readAMG8833ThermistorBytes(&rawGridData[0]);
+    TWI_readAMG8833Bytes(AMG8833_T01L, AMG8833_GRID_BYTES, &rawGridData[0]);
     for(uint16_t ii = 0; ii < 64; ii++) {
         tempValues[ii] = (float) ((int16_t) ( (int16_t) rawGridData[2*ii + 1] << 8) | rawGridData[2*ii]);
-        tempValues[ii] *=0.25f; // scale to get temperatures in degrees C
+        tempValues[ii] *=0.25f; // scale to get temperatures in degrees C. Refer to data sheet "Temperature Output Resolution"
     }
-
+    
+    return tempValues;
 }
 
 
-int TWI_readAMG8833ThermistorBytes(uint8_t * dest) {
+int TWI_readAMG8833Bytes(uint8_t reg, uint8_t len, uint8_t * dest) {
         
     uint8_t twcr, twst = 0;
     int bytesReceived = 0;
@@ -144,11 +175,11 @@ int TWI_readAMG8833ThermistorBytes(uint8_t * dest) {
 
     // Check value of TWI Status Register. Mask prescaler bits. If status different from MT_SLA_ACK go to ERROR
     if ((TWSR & 0xF8) != TW_MT_SLA_ACK) LCD_sendDataString("ERROR: B");
-    else goto stop;
+    else goto stopSignal;
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////
     // Load Register Address in device into TWDR Register
-    TWDR = AMG8833_TTHL;
+    TWDR = reg;
 
     // Clear TWINT bit in TWCR to start transmission of data
     TWCR = (1<<TWINT) | (1<<TWEN);
@@ -158,7 +189,7 @@ int TWI_readAMG8833ThermistorBytes(uint8_t * dest) {
 
     // Check value of TWI Status Register. Mask prescaler bits. If status different from MT_DATA_ACK go to ERROR
     if ((TWSR & 0xF8) != TW_MT_DATA_ACK) LCD_sendDataString("ERROR: C");
-    else goto stop;
+    else goto stopSignal;
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////
     // Repeated Start Condition
@@ -169,7 +200,7 @@ int TWI_readAMG8833ThermistorBytes(uint8_t * dest) {
 
     // Check value of TWI Status Register. Mask prescaler bits. If status different from REP_START go to ERROR
     if ((TWSR & 0xF8) != TW_REP_START) LCD_sendDataString("ERROR: D");
-    else goto stop;
+    else goto stopSignal;
 
     ///////////////////////////////////////////////////////////////////////////////////////////////////
     // Write SLA+R (means Slave Address + Read-Flag) into TWI Data Register => entering Master Receiver Mode
@@ -183,11 +214,10 @@ int TWI_readAMG8833ThermistorBytes(uint8_t * dest) {
 
     // Check value of TWI Status Register. Mask prescaler bits. If status different from MR_SLA_ACK go to ERROR
     if ((TWSR & 0xF8) != TW_MR_SLA_ACK) LCD_sendDataString("ERROR: E");
-    else goto stop;
-        
-    // helper variables
-    uint8_t len = AMG8833_THERMISTOR_BYTES;
-
+    else goto stopSignal;
+    
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+    // receive Data
     // TWEA - TWI Enable Acknowledge Bit (setting this bit will send back ACK as confirmation of receiving a byte)
     for (twcr = (1 << TWINT) | (1 << TWEN) | (1 << TWEA); len > 0; len--)
     {
@@ -211,18 +241,18 @@ int TWI_readAMG8833ThermistorBytes(uint8_t * dest) {
             case TW_MR_DATA_ACK:
                 *dest++ = TWDR;
                 bytesReceived++;
-                if(twst == TW_MR_DATA_NACK) goto stop;
+                if(twst == TW_MR_DATA_NACK) goto stopSignal;
                 break;
             default:
                 LCD_sendDataString("ERROR: F");
-                goto stop;
+                goto stopSignal;
         }
             
     }
         
     ///////////////////////////////////////////////////////////////////////////////////////////////////
     // Stop Condition
-    stop:
+    stopSignal:
     TWCR = (1 << TWINT) | (1 << TWSTO) | (1 << TWEN);
     LCD_setCursorTo(0, 2);
     LCD_sendDataString("Sent STOP");
