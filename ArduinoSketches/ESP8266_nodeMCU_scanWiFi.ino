3243d6c37c8b7da76d6ff18d8ac51a6a31e84b61
michelheil <49286214+michelheil@users.noreply.github.com>
2019-10-11 21:54:38 +0200

    Create ESP8266_nodeMCU_scanWiFi.ino

---
+++ ArduinoSketches/ESP8266_nodeMCU_scanWiFi.ino
@@ -0,0 +1,26 @@
+#include "ESP8266WiFi.h"
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+}
+
+void loop()
+{
+  Serial.print("Scan start ... ");
+  int n = WiFi.scanNetworks();
+  Serial.print(n);
+  Serial.println(" network(s) found");
+  for (int i = 0; i < n; i++)
+  {
+    Serial.println(WiFi.SSID(i));
+  }
+  Serial.println();
+
+  delay(5000);
+}
