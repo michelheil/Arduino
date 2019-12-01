51af5f600e42108f18ed99c6601f398e72aaaeb3
Michael <michelheil@gmx.de>
2019-12-01 21:03:34 +0100

    added License to header files

--- VisualStudio/ArduinoUno/include/myBME680.h
+++ VisualStudio/ArduinoUno/include/myBME680.h
@@ -5,6 +5,23 @@
  *        Temperature: -40 - +85 Celcius
  *        Humidity: 0 - 100% r.H. (relative Humidity)
  *        Pressure: 300 - 1100 hPa (hecto Pascal)
+ * 
+    Copyright (C) 2019  Michael Heil
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * 
 Data Sheet: https://ae-bst.resource.bosch.com/media/_tech/media/datasheets/BST-BME680-DS001.pdf
 Bosch BME680_Driver: https://github.com/BoschSensortec/BME680_driver
 */
--- VisualStudio/ArduinoUno/include/myLCD.h
+++ VisualStudio/ArduinoUno/include/myLCD.h
@@ -1,7 +1,21 @@
 /*
- * myLCD.h
+    Programm to sending data to a 1602 LCD in 4-bit mode
+    Copyright (C) 2019  Michael Heil
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
- * library to sending data to a 1602 LCD in 4-bit mode
+ * 
  *
  * Created: 26.09.2019 20:58:42
  * Author : Michael
