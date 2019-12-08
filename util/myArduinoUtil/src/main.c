b9411b251d120c31c43749b6031608cce4267f00
Michael <michelheil@gmx.de>
2019-12-08 09:32:20 +0100

    moved myArduinoLibrary to myArduinoUtil

---
+++ util/myArduinoUtil/src/main.c
@@ -0,0 +1,19 @@
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// compile this project
+// go to .pio/build/uno/src
+// open folder in terminal
+// rm libmyArduinoUtil.a
+// avr-ar rcs libmyArduinoUtil.a myADC.c.o myGlobalDefines.c.o myLOG.c.o myTWI.c.o myUSART.c.o --plugin=$(gcc --print-file-name=liblto_plugin.so)
+// check content of library with
+// ar -cvq libmyArduinoUtil.a *.o
+// ar -t libmyArduinoUtil.a
+//
+// To use the library, insert the following line to platformio.ini file
+// build_flags = -L/home/michael/GitHubRepositories/Arduino/util/myArduinoUtil/.pio/build/uno/src -lmyArduinoUtil -I/home/michael/GitHubRepositories/Arduino/util/myArduinoUtil/include
