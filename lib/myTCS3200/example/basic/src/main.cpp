59842c9b81da42c6c30daefcb46138d19af2d3c7
Michael <michelheil@gmx.de>
2019-12-23 09:10:41 +0100

    refactoring of variable names without touching interrupts

--- lib/myTCS3200/example/basic/src/main.cpp
+++ lib/myTCS3200/example/basic/src/main.cpp
@@ -8,168 +8,153 @@
 #include "myGlobalDefines.h"
 
 #include <TimerOne.h>
-#define S0 6
-#define S1 5
-#define S2 4
-#define S3 3
-#define OUT 2
+#include <util/delay.h>
 
-int g_count = 0;
-int g_array[3];
-int g_flag = 0;
-float g_SF[3];
-void TSC_Init()
+#define SET_BIT_LEVEL(PORT, bit, level) ( ((level) == 0) ? (cbi(PORT, bit)) : (sbi(PORT, bit)) )
+
+#define TCS3200_S0  PD6 
+#define TCS3200_S1  PD5
+#define TCS3200_S2  PD4
+#define TCS3200_S3  PD3
+#define TCS3200_OUT PD2 // INT0
+
+struct rgb {
+
+  // stores the tscOutCount for each color (red, green, blue)
+  int red;
+  int green;
+  int blue;
+
+  // calibration factors for each color (red, green, blue)
+  float redCalibFact;
+  float greenCalibFact;
+  float blueCalibFact;
+};
+
+struct rgb tscColors;
+int tscOutCount = 0; // INT0 counter; hits on all rising edges
+int colorFilterSwitch = 0; // color filter switch; switches every time the Timer hits the compare value
+
+void TSC3200_init(void)
 {
-  pinMode(S0, OUTPUT);
-  pinMode(S1, OUTPUT);
-  pinMode(S2, OUTPUT);
-  pinMode(S3, OUTPUT);
-  pinMode(OUT, INPUT);
+  // set data direction registers
+  sbi(DDRD, TCS3200_S0);
+  sbi(DDRD, TCS3200_S1);
+  sbi(DDRD, TCS3200_S2);
+  sbi(DDRD, TCS3200_S3);
+  cbi(DDRD, TCS3200_OUT);
 
-  digitalWrite(S0, LOW);
-  digitalWrite(S1, HIGH);
+  /* Output Frequency Sclaing (f_0)
+  S0  S1  Output Frequency Scaling (f_0)
+  L   L   Power down
+  L   H   2%
+  H   L   20%
+  H   H   100%
+  */
+  cbi(PORTD, TCS3200_S0);
+  sbi(PORTD, TCS3200_S1);
 }
 
 
-void TSC_FilterColor(int Level01, int Level02)
+void TSC3200_colorSelection(int Level01, int Level02)
 {
-  if(Level01 != 0)
-  Level01 = HIGH;
-
-  if(Level02 != 0)
-  Level02 = HIGH;
-
-  digitalWrite(S2, Level01);
-  digitalWrite(S3, Level02);
+  /*
+  S2  S3  Photodiode Type
+  L   L   Red
+  L   H   Blue
+  H   L   Clear (no filter)
+  H   H   Green
+  */
+  SET_BIT_LEVEL(PORTD, TCS3200_S2, Level01);
+  SET_BIT_LEVEL(PORTD, TCS3200_S3, Level02);
 }
 
-void TSC_Count()
+void TSC3200_count()
 {
-  g_count ++ ;
+  // count rising edges on INT0
+  // this reflects the frequency for an applied filter
+  tscOutCount ++;
 }
 
 
 
 void TSC_WB(int Level0, int Level1)
 {
-  g_count = 0;
-  g_flag ++;
-  TSC_FilterColor(Level0, Level1);
+  // set INTO Interrupt counter to 0
+  tscOutCount = 0;
+
+  // ensure that next time another color filter is applied
+  colorFilterSwitch ++;
+
+  // select color filter
+  TSC3200_colorSelection(Level0, Level1);
+
+  // count for 1 second (= 1000000 microsends)
+  // during that time the INT0 interrupt will use 
+  // g_count to count rising edges
   Timer1.setPeriod(1000000);
 }
 
 
-
 void TSC_Callback()
 {
-  switch(g_flag)
+  switch(colorFilterSwitch)
   {
     case 0:
       Serial.println("->WB Start");
-      TSC_WB(LOW, LOW); // Filter ohne Rot
+      TSC_WB(LOW, LOW); // Filter Rot
+      Serial.print("->Frequency R=");
+      Serial.println(tscOutCount);
+      tscColors.red = tscOutCount;
       break;
     case 1:
-      Serial.print("->Frequency R=");
-      Serial.println(g_count);
-      g_array[0] = g_count;
-      TSC_WB(HIGH, HIGH); // Filter ohne Grün
+      TSC_WB(HIGH, HIGH); // Filter Grün
+      Serial.print("->Frequency G=");
+      Serial.println(tscOutCount);
+      tscColors.green = tscOutCount;
       break;
     case 2:
-      Serial.print("->Frequency G=");
-      Serial.println(g_count);
-      g_array[1] = g_count;
-      TSC_WB(LOW, HIGH); // Filter ohne Blau
+      TSC_WB(LOW, HIGH); // Filter Blau
+      Serial.print("->Frequency B=");
+      Serial.println(tscOutCount);
+      Serial.println("->WB End");
+      tscColors.blue = tscOutCount;
       break;
     case 3:
-      Serial.print("->Frequency B=");
-      Serial.println(g_count);
-      Serial.println("->WB End");
-      g_array[2] = g_count;
       TSC_WB(HIGH, LOW); // Kein Filter
       break;
     default:
-      g_count = 0;
+      tscOutCount = 0;
       break;
   }
 }
 
-
-
-
-void setup()
+int main(void)
 {
-  TSC_Init();
+  TSC3200_init();
   Serial.begin(9600);
   Timer1.initialize();
   Timer1.attachInterrupt(TSC_Callback);
-  attachInterrupt(0, TSC_Count, RISING);
-  delay(4000);
-  for(int i=0; i<3; i++)
-    Serial.println(g_array[i]);
+  attachInterrupt(0, TSC3200_count, RISING); // count rising edges on INTO (Pin 0)
+  _delay_ms(4000);
+  Serial.println(tscColors.red);
+  Serial.println(tscColors.green);
+  Serial.println(tscColors.blue);
 
   // calibration on white background
-  g_SF[0] = 255.0/ g_array[0]; //R-Wert (calibration factor, such that 255 is maximum)
-  g_SF[1] = 255.0/ g_array[1] ; //G-Wert
-  g_SF[2] = 255.0/ g_array[2] ; //B-Wert
-  Serial.println(g_SF[0]);
-  Serial.println(g_SF[1]);
-  Serial.println(g_SF[2]);
-}
-
-void loop()
-{
-  g_flag = 0;
-  for(int i=0; i<3; i++)
-    Serial.println(int(g_array[i] * g_SF[i]));
-  delay(4000);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-#include <util/delay.h>
-#include "myTCS3200.h"
-
-#include "myUSART.h"
-
-int main(void)
-{
-  // Initialize Serial (USART)
-  USART_init();
-
-  pinMode
+  tscColors.redCalibFact = 255.0/ tscColors.red; // R-Wert (calibration factor, such that 255 is maximum)
+  tscColors.greenCalibFact = 255.0/ tscColors.green; // G-Wert
+  tscColors.blueCalibFact = 255.0/ tscColors.blue; // B-Wert
+  Serial.println(tscColors.redCalibFact);
+  Serial.println(tscColors.greenCalibFact);
+  Serial.println(tscColors.blueCalibFact);
 
   while(1)
   {
-    _delay_ms(2000);
-    USART_newLine();
+    colorFilterSwitch = 0;
+    _delay_ms(4000);
+    Serial.println(int(tscColors.redCalibFact * tscColors.red));
+    Serial.println(int(tscColors.greenCalibFact * tscColors.green));
+    Serial.println(int(tscColors.blueCalibFact * tscColors.blue));
   }
 }
-*/
