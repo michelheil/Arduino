157bd3521488ef323bbbccf97164808bed108335
michelheil <49286214+michelheil@users.noreply.github.com>
2020-01-19 18:28:51 +0100

    comments for BOD disable

--- misc/mySleepMain.c
+++ misc/mySleepMain.c
@@ -46,6 +46,15 @@ int main(void)
     // "sleep" for c. 64 seconds
     for(int ii = 0; ii < 8; ii++)
     {
+      // If Brown-Out Detector (BOD) is disabled by Software, the BOD function is turned off immediately after entering the
+      // sleep mode. Upon wake-up from sleep, BOD is automatically enabled again.
+      // When the BOD has been disabled, the wake-up time from sleep mode will be approximately 60us to ensure that the BOD
+      // is working correctly before the MCU continues executing code.
+      //
+      // In order to disable BOD during sleep the BODS bit must be written to logic one. First, the BODS and BODSE must be 
+      // set to one. Second, within four clock cycles, BODS must be set to one and BODSE must be set to zero. The BODS bit 
+      // is active three clock cycles after it is set. A sleep instruction must be executed while BODS is active in order
+      // to turn off the BOD for the actual sleep mode. The BODS bit is automatically cleared after three clock cycles.
       MCUCR |= (1 << BODS) | (1 << BODSE); // set both BODS and BODSE at the same time
       MCUCR = (MCUCR & ~(1 << BODSE)) | (1 << BODS); // then set the BODS bit and clear the BODSE bit at the same time
       __asm__ __volatile__("sleep");
